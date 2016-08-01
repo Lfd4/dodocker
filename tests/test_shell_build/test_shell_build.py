a003497d4f72280a9649816ea68645f868ae9503
Andreas Elvers <andreas@work.de>
2016-08-01 22:15:19 +0200

    [TESTING] moving to test methods

--- tests/test_shell_build/test_shell_build.py
+++ tests/test_shell_build/test_shell_build.py
@@ -1,10 +1,11 @@
 from dodocker import run_dodocker_cli
 import os, pytest
 
-def test_build(tmpdir_copy):
-    with tmpdir_copy.as_cwd():
-        run_dodocker_cli(['-o','output.txt','build'])
-    output = tmpdir_copy.join('output.txt').open().read()
-    assert output == '\n'.join(('.  build_dodockertest/shtest1',
-                                '.  build_dodockertest/shtest2',
-                                ''))
+class TestShellBuild:
+    def test_build(self,tmpdir_copy):
+        with tmpdir_copy.as_cwd():
+            run_dodocker_cli(['-o','output.txt','build'])
+        output = tmpdir_copy.join('output.txt').open().read()
+        assert output == '\n'.join(('.  build_dodockertest/shtest1',
+                                    '.  build_dodockertest/shtest2',
+                                    ''))
