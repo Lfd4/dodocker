318cf509a226b1281b049929b43635465fc9bb33
Andreas Elvers <andreas@work.de>
2016-08-03 17:15:13 +0200

    [TESTING] fixed image deletion

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from distutils.dir_util import copy_tree
 import os, hashlib, subprocess
 
-@pytest.yield_fixture(scope='class')
+@pytest.yield_fixture(scope='module')
 def tmpdir_copy(tmpdir_factory, request):
     tmpdir_name = hashlib.md5(str(request.node)).hexdigest()
     t = tmpdir_factory.mktemp(tmpdir_name)
@@ -12,20 +12,26 @@ def tmpdir_copy(tmpdir_factory, request):
     yield t
 
 def find_and_delete_testimages():
-    images = " ".join(subprocess.check_output(
-        "docker images | grep dodockertest | awk '{print $1}'", shell=True
-        ).split())
+    images = set(subprocess.check_output(
+        "docker images | grep dodockertest | awk '{print $3}'", shell=True).split())
     if not images:
         return
-    subprocess.check_call(["docker rmi {}".format(images)], shell=True)
+    done = set()
+    for mode in ('','-f'):
+        for i in images:
+            try:
+                subprocess.check_call(["docker rmi {} {}".format(mode,i)], shell=True)
+            except subprocess.CalledProcessError:
+                continue
+            done.add(i)
+        images = images - done
+            
     
 @pytest.yield_fixture(scope='session', autouse=True)
 def on_session_start_end_clean_docker_from_testimages():
     find_and_delete_testimages()
     yield
     find_and_delete_testimages()
-
-
     
 @pytest.yield_fixture(scope='session')
 def docker_registry(scope='session'):
