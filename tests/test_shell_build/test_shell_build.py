5c3b8c7e53c0a1d6764f6d551d8c98db8044c11a
Andreas Elvers <andreas@work.de>
2016-08-04 12:29:04 +0200

    [TESTING] dependency on file modification for shell build

--- tests/test_shell_build/test_shell_build.py
+++ tests/test_shell_build/test_shell_build.py
@@ -9,3 +9,21 @@ class TestShellBuild:
         assert output == '\n'.join(('.  build_dodockertest/shtest1',
                                     '.  build_dodockertest/shtest2',
                                     ''))
+
+    def test_2nd_build(self,tmpdir_copy):
+        with tmpdir_copy.as_cwd():
+            run_dodocker_cli(['-o','output.txt','build'])
+        output = tmpdir_copy.join('output.txt').open().read()
+        assert output == '\n'.join(('-- build_dodockertest/shtest1',
+                                    '-- build_dodockertest/shtest2',
+                                    ''))
+
+    def test_3d_build_with_touch(self, tmpdir_copy):
+        tmpdir_copy.join('image1','test.sh').open('a').write('\n')
+        with tmpdir_copy.as_cwd():
+            run_dodocker_cli(['-o','output.txt','build'])
+        output = tmpdir_copy.join('output.txt').open().read()
+        assert output == '\n'.join(('-- build_dodockertest/shtest1',
+                                    '-- build_dodockertest/shtest2',
+                                    ''))
+        
