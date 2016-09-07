f1e3beb5c4e0aa6b0599433ac1608660ea0767af
Andreas Elvers <andreas@work.de>
2016-09-07 10:17:53 +0200

    fixes enconfing for python3

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import os, hashlib, subprocess
 
 @pytest.yield_fixture(scope='module')
 def tmpdir_copy(tmpdir_factory, request):
-    tmpdir_name = hashlib.md5(str(request.node)).hexdigest()
+    tmpdir_name = hashlib.md5(str(request.node).encode('utf-8')).hexdigest()
     t = tmpdir_factory.mktemp(tmpdir_name)
     with t.as_cwd():
         copy_tree(os.path.dirname(request.module.__file__),
